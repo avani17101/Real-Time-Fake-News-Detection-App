4dc5ace94d20f074961c73a16929a4f1601367f2
avani_gupta <avani.gupta@research.iiit.ac.in>
2020-02-29 22:30:00 +0530

    changed login singup in Scapshut-start

--- Scrapshut/Code/Scrapshut-start/src/app/app-routing.module.ts
+++ Scrapshut/Code/Scrapshut-start/src/app/app-routing.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {RateWebsiteForm} from './RateWebsiteForm.component'
 import {StartPage} from './start-page.component'
-import {LoginComponent} from './login/login.component'
+
 
 
 const routes: Routes = [
   { path : '', component: StartPage},
   { path: 'RatingForm', component: RateWebsiteForm},
-  { path: 'LoginForm', component:LoginComponent}
 ];
 
 @NgModule({
